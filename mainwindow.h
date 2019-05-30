25b71f6f5f0266564bd88a190bb71da5ea573f2f
hooray <hooray1998@foxmail.com>
2019-05-30 20:44:30 +0800

    init，都ok

---
+++ mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QRadioButton>
+#include "studentform.h"
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QMessageBox>
+#include<manger.h>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_LoginButton_clicked();
+    void on_remitButton_clicked();
+
+signals:
+    void sendData(QString);
+private:
+    Ui::MainWindow *ui;
+    studentForm *f;
+    manger *m;
+};
+
+#endif // MAINWINDOW_H
